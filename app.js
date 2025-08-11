84ecdea27c4317c00274e911e480ad6892fe6b7a
Isabelle Fonseca <isabelle7536@gmail.com>
2025-08-11 02:07:40 -0300

    exibir lista de amigos na tela

--- app.js
+++ app.js
@@ -22,3 +22,27 @@ function adicionarAmigo() {
 
     input.value = "";
 }
+
+// função para atualizar a lista de amigos na tela
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+
+        let span = document.createElement("span");
+        span.textContent = amigos[i];
+
+        let btnRemover = document.createElement("button");
+        btnRemover.textContent = "❌";
+        btnRemover.classList.add("button-remove");
+        btnRemover.onclick = function () {
+            removerAmigo(i);
+        };
+
+        li.appendChild(span);
+        li.appendChild(btnRemover);
+        lista.appendChild(li);
+    }
+}
