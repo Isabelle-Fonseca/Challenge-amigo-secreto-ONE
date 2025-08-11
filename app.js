29eb0be9f53cbd777af46d9f8b7bc26b34629a1e
Isabelle Fonseca <isabelle7536@gmail.com>
2025-08-11 01:56:34 -0300

    função para adicionar amigos com validações

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 let amigos = [];
+
+// função para adicionar amigo
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    if (amigos.some(a => a.toLowerCase() === nome.toLowerCase())) {
+        alert("Esse nome já foi adicionado.");
+        input.value = "";
+        return;
+    }
+
+    amigos.push(nome);
+
+    atualizarLista();
+
+    input.value = "";
+}
